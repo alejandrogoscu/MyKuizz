7a2b7daaed37cf1f8de94187ff4da358722e90a5
alejandrogoscu <alejandrogoscu@gmail.com>
2025-05-13 20:32:43 +0200

    Logica de JS para crear boton de las respuestas

--- main.js
+++ main.js
@@ -2,15 +2,14 @@
 const bienvenida = document.getElementById("bienvenida")
 const questionario = document.getElementById("questionario")
 const pregunta = document.getElementById("pregunta")
+const botonesRespuesta = document.getElementById("botones-respuesta")
 const contenedorSec = document.getElementById("contenedor-sec")
 const contador = document.getElementById("contador")
 
 const btnEmpezar = document.getElementById("btn-empezar")
-const btnRespuestaA = document.getElementById("option-a")
 const btnNext = document.getElementById("btn-next")
 const btnStats = document.getElementById("btn-stats")
 const btnSalir = document.getElementById("btn-salir")
-console.log(btnRespuestaA)
 
 let currentQuestionIndex;
 let score = 0
@@ -56,7 +55,10 @@ function initQuestions() {
 }
 
 function showRespuesta(correcta) {
-  btnRespuestaA.innerText = correcta
+  const options = document.createElement("button")
+  options.classList.add("btn_respuesta")
+  options.innerText = correcta
+  botonesRespuesta.appendChild(options)
 }
 
 function showPregunta(question) {
