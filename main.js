786db85b2ca88087a2bd4cce4ed433dedccf2701
alejandrogoscu <alejandrogoscu@gmail.com>
2025-05-13 18:12:04 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+
+const bienvenida    = document.getElementById("bienvenida")
+const questionario  = document.getElementById("questionario")
+const pregunta      = document.getElementById("pregunta")
+const contador      = document.getElementById("contador")
+
+const btnEmpezar    = document.getElementById("btn-empezar")
+const btnRespuesta  = document.getElementById("btn-respuesta")
+const btnNext       = document.getElementById("btn-next")
+const btnStats      = document.getElementById("btn-stats")
+const btnSalir      = document.getElementById("btn-salir")
