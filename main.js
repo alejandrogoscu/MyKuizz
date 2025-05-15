db1e9f6bd98cf88323c9f84359f92e54ae721789
alejandrogoscu <alejandrogoscu@gmail.com>
2025-05-15 20:17:13 +0200

    Acabada la logica basica del quiz

--- main.js
+++ main.js
@@ -3,12 +3,16 @@ const API_URL = "https://opentdb.com/api.php?amount=10&type=multiple"
 const bienvenida = document.getElementById("bienvenida")
 const questionario = document.getElementById("questionario")
 const pregunta = document.getElementById("pregunta")
-const botonesRespuesta = document.getElementById("botones-respuesta")
+const resultadoDiv = document.getElementById("resultado")
 const contenedorSec = document.getElementById("contenedor-sec")
 const contador = document.getElementById("contador")
 
+
+
+
 const btnEmpezar = document.getElementById("btn-empezar")
-const btnNext = document.getElementById("btn-next")
+const botonesRespuesta = document.getElementById("botones-respuesta")
+const btnSiguiente = document.getElementById("btn-next")
 const btnStats = document.getElementById("btn-stats")
 const btnSalir = document.getElementById("btn-salir")
 
@@ -19,18 +23,24 @@ let preguntas = [];
 let correctas = [];
 let incorrectas = [];
 
-
-// funciones <hide> class.
+// Decofificar carácteres de la API.
+function decodeHTMLEntities(text) {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(text, 'text/html');
+  return doc.documentElement.textContent;
+}
+// funciones para ocultar y borrar.
 function ocultarTodo() {
   bienvenida.classList.add("hide")
   questionario.classList.add("hide")
   contenedorSec.classList.add("hide")
-  // btnNext.classList.add("hide")
+  btnSiguiente.classList.add("hide")
   btnSalir.classList.add("hide")
 }
 function irBienvenida() {
   ocultarTodo()
   bienvenida.classList.remove("hide")
+  contador.innerText = "1/10"
 }
 function irQuestionario() {
   ocultarTodo()
@@ -38,29 +48,64 @@ function irQuestionario() {
   contenedorSec.classList.remove("hide")
   btnSalir.classList.remove("hide")
 }
-// Decofificar carácteres de la API.
-function decodeHTMLEntities(text) {
-  const parser = new DOMParser();
-  const doc = parser.parseFromString(text, 'text/html');
-  return doc.documentElement.textContent;
+function borrarDivs() {
+  botonesRespuesta.innerHTML = ""
+  resultadoDiv.innerHTML = ""
 }
 
 
 
+// Lógica del Quiz.
 function mostrarPregunta(question) {
   pregunta.innerText = decodeHTMLEntities(question)
 }
 
 function mostrarRespuestas(incorrectas, correcta) {
-  botonesRespuesta.innerHTML = ""
+  borrarDivs()
   let todasPreguntas = [...incorrectas, correcta]
+  todasPreguntas.sort(() => Math.random() - 0.5)
   todasPreguntas.forEach(element => {
     const options = document.createElement("button")
     options.classList.add("btn_respuesta")
     options.innerText = decodeHTMLEntities(element)
+    if (correctas.includes(options.innerText)) {
+      options.dataset.correct = true
+    }
+      
     botonesRespuesta.appendChild(options)
   });
 }
+
+function mostrarResCorrecta() {
+  const resultado = document.createElement("h2")
+  resultado.classList.add("texto_correcta")
+  resultado.innerText = "¡Correcta!"
+  resultadoDiv.appendChild(resultado)
+}
+function mostrarResFallida() {
+  const resultado = document.createElement("h2")
+  resultado.classList.add("texto_fallida")
+  resultado.innerText = "¡Esta vez fallaste!"
+  resultadoDiv.appendChild(resultado)
+}
+function validarRespuesta(element) {
+  if (element.dataset.correct) {
+    mostrarResCorrecta()
+    element.classList.add("correcta")
+    puntuacion += 10
+    console.log(puntuacion)
+  } else {
+    mostrarResFallida()
+    element.classList.add("fallida")
+    const restoRespuestas = document.querySelectorAll(".btn_respuesta")
+    restoRespuestas.forEach(respuesta => {
+      if (respuesta.dataset.correct) {
+        respuesta.classList.add("correcta")
+      }
+    })
+  }
+}
+
 
 async function obtenerPreguntas() {
   try {
@@ -68,36 +113,48 @@ async function obtenerPreguntas() {
     const questionList = question.data.results
 
     preguntas = questionList.map(question => question.question);
-    correctas = questionList.map(correcta => correcta.correct_answer)
+    correctas = questionList.map(correcta => decodeHTMLEntities(correcta.correct_answer))
     incorrectas = questionList.map(respuestas => respuestas.incorrect_answers)
+    incorrectas = incorrectas
 
     preguntaActual = 0
+    puntuacion = 0
     mostrarPregunta(preguntas[preguntaActual])
     mostrarRespuestas(incorrectas[preguntaActual], correctas[preguntaActual])
     irQuestionario()
 
-    console.log(preguntas)
-    console.log(correctas)
-    console.log(incorrectas)
   } catch (error) {
     console.error(error)
   }
 }
 
 
-function siguientePregunta() {
-  preguntaActual++
 
-  mostrarPregunta(preguntas[preguntaActual])
-  mostrarRespuestas(incorrectas[preguntaActual], correctas[preguntaActual])
+function responder(e) {
+  document.querySelectorAll(".btn_respuesta").forEach(option => option.classList.add("clicado"))
+  btnSiguiente.classList.remove("hide")
+  const respuesta = e.target
+  validarRespuesta(respuesta)
 }
 
 
-
+function siguientePregunta() {
+  if (preguntas.length > preguntaActual + 1) {
+    preguntaActual++
+    const numContador = preguntaActual + 1
+    contador.innerText = `${numContador}/10`
+    btnSiguiente.classList.add("hide")
+    mostrarPregunta(preguntas[preguntaActual])
+    mostrarRespuestas(incorrectas[preguntaActual], correctas[preguntaActual])
+  } else {
+    console.log("se acabaron las preguntas")
+  }
+}
 
 
 
 // EVENTOS.
 btnSalir.addEventListener("click", irBienvenida)
 btnEmpezar.addEventListener("click", obtenerPreguntas)
-btnNext.addEventListener("click", siguientePregunta)
+botonesRespuesta.addEventListener("click", responder)
+btnSiguiente.addEventListener("click", siguientePregunta)
